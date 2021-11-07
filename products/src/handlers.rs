2bf40bb017bbd93ccb9622ba7d6890e312b2dafb
czar <czar.yobero@gmail.com>
2021-11-06 18:04:53 -0700

    factor handlers

--- products/src/handlers.rs
+++ products/src/handlers.rs
@@ -13,20 +13,38 @@ pub async fn post_product(
     let conn = pool.get().expect("Could not get connection from pool.");
     web::block(move || form.into_inner().create(&conn))
         .await
-        .map(|prod| HttpResponse::Ok().json(prod))
-        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+        .map(|prod| HttpResponse::Ok().json(json!({"status": 200, "data": prod})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
+}
+
+#[get("/products")]
+pub async fn get_products(pool: web::Data<DbPool>) -> Result<HttpResponse, HttpResponse> {
+    let conn = pool.get().expect("Could not get connection from pool.");
+    web::block(move || Product::all(&conn))
+        .await
+        .map(|prods| HttpResponse::Ok().json(json!({"status": 200, "data": prods})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
 }
 
 #[get("/products/{id}")]
 pub async fn get_product_id(
     pool: web::Data<DbPool>,
-    path: web::Path<(i32,)>,
+    path: web::Path<i32>,
 ) -> Result<HttpResponse, HttpResponse> {
     let conn = pool.get().expect("Could not get connection from pool.");
-    web::block(move || Product::with_id(&conn, &path.into_inner().0))
+    web::block(move || Product::with_id(&conn, &path.into_inner()))
         .await
-        .map(|prod| HttpResponse::Ok().json(prod))
-        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+        .map(|prod| HttpResponse::Ok().json(json!({"status": 200, "data": prod})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
 }
 
 #[post("/products/cannabis")]
@@ -37,23 +55,29 @@ pub async fn post_cannabis(
     let conn = pool.get().expect("Could not get connection from pool.");
     web::block(move || form.into_inner().create(&conn))
         .await
-        .map(|cnbs| HttpResponse::Ok().json(cnbs))
-        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+        .map(|cnbs| HttpResponse::Ok().json(json!({"status": 200, "data": cnbs})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
 }
 
 #[get("/products/cannabis/{id}")]
 pub async fn get_cannabis_id(
     pool: web::Data<DbPool>,
-    path: web::Path<(i32,)>,
+    path: web::Path<i32>,
 ) -> Result<HttpResponse, HttpResponse> {
     let conn = pool.get().expect("Could not get connection from pool.");
-    web::block(move || Cannabis::with_id(&conn, &path.into_inner().0))
+    web::block(move || Cannabis::with_id(&conn, &path.into_inner()))
         .await
-        .map(|cnbs| HttpResponse::Ok().json(cnbs))
-        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+        .map(|cnbs| HttpResponse::Ok().json(json!({"status": 200, "data": cnbs})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
 }
 
-#[post("/products/inventories")]
+#[post("/inventories")]
 pub async fn post_inventory(
     pool: web::Data<DbPool>,
     form: web::Form<NewInventory>,
@@ -61,39 +85,36 @@ pub async fn post_inventory(
     let conn = pool.get().expect("Could not get connection from pool.");
     web::block(move || form.into_inner().create(&conn))
         .await
-        .map(|inv| HttpResponse::Ok().json(inv))
-        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+        .map(|inv| HttpResponse::Ok().json(json!({"status": 200, "data": inv})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
 }
 
 #[get("/products/{id}/inventory")]
 pub async fn get_product_inventory(
     pool: web::Data<DbPool>,
-    path: web::Path<(i32,)>,
+    path: web::Path<i32>,
 ) -> Result<HttpResponse, HttpResponse> {
     let conn = pool.get().expect("Could not get connection from pool.");
-    web::block(move || Inventory::with_product_id(&conn, &path.into_inner().0))
+    web::block(move || Inventory::with_product_id(&conn, &path.into_inner()))
         .await
-        .map(|inv| HttpResponse::Ok().json(inv))
-        .map_err(|e| HttpResponse::InternalServerError().json(e.to_string()))
+        .map(|inv| HttpResponse::Ok().json(json!({"status": 200, "data": inv})))
+        .map_err(|e| {
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
+        })
 }
 
-#[get("/products/inventories/")]
+#[get("/inventories")]
 pub async fn get_inventories(pool: web::Data<DbPool>) -> Result<HttpResponse, HttpResponse> {
     let conn = pool.get().expect("Could not get connection from pool.");
     web::block(move || InventoryResponse::all(&conn))
         .await
-        .map(|inv| {
-            let data = json!({
-                "Status": 200,
-                "Data": inv
-            });
-            HttpResponse::Ok().json(&data)
-        })
+        .map(|inv| HttpResponse::Ok().json(json!({"status": 200, "data": inv})))
         .map_err(|e| {
-            let data = json!({
-                "Status": 404,
-                "Message": e.to_string()
-            });
-            HttpResponse::InternalServerError().json(&data)
+            HttpResponse::InternalServerError()
+                .json(json!({"status": 404, "message": e.to_string()}))
         })
 }
